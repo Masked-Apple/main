1c6e58ffc748bedd6cd91655ba317932d77b4c20
Apple <50032656+Masked-Apple@users.noreply.github.com>
2019-05-28 02:16:15 +0100

    Add files via upload

---
+++ cog.py
@@ -0,0 +1,27 @@
+
+import discord
+from discord.ext import commands
+import libneko
+
+
+prefix = "!"
+Client = discord.Client()
+client = commands.Bot(command_prefix = '!')
+owner = int(303152083891257344)
+winners = []
+
+
+@client.event
+async def on_ready():
+    print("✔ Connected!\n")
+    ez = client.load_extension("libneko.extras.superuser")
+    print("Loaded cog")
+
+
+
+
+
+token = "NDgwMDQzMTEwNjgwODg3MzM2.DrH4lA.e96wOyoGm9-C6yWnnUHbMsM-zRA"
+
+
+client.run(token)
